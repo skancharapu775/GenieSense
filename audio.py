e7eda9d248fd507f97815b9d60a3170ebc4eb974
sid.kancharapu@gmail.com <siddharth.kancharapu@icloud.com>
2023-04-15 10:21:58 -0400

    Test audio conversion

--- audio.py
+++ audio.py
@@ -1,7 +1,18 @@
 """
 Find fast way to convert text to sound
+pip install gTTS
 """
+from gtts import *
+import os
+
+LANGUAGES = ['en']
+NAME = "speech.mp3"
 
 # turns text into audio file
-def generate_file(text):
-    return NotImplementedError
+def generate_file(text, language):
+    obj = gTTS(text=text, lang=language, slow=False)
+    obj.save(NAME)
+    
+    return NAME
+
+generate_file("hello there", LANGUAGES[0])
